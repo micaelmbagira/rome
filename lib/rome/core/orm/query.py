36bc5b179487725906ebf20d3aebd03a242bf0a8
Micaël <micael.mbagira@icloud.com>
2015-12-15 23:09:54 +0100

    update function returns number of results

--- lib/rome/core/orm/query.py
+++ lib/rome/core/orm/query.py
@@ -129,6 +129,7 @@ class Query:
                     self._session.add(each)
             except:
                 pass
+        return len(result)
 
     def distinct(self):
         return list(set(self.all()))
